520b045b4307cdd7f2955d48e5866733b2d6b6b5
Elena Kovalenko <aszhigalova@gmail.com>
2022-11-16 18:43:20 +1100

    completed exercise 3, week 14

---
+++ week 14/task3.js
@@ -0,0 +1,9 @@
+function next() {
+    let image = document.getElementById('photo');
+    image.src = "https://s9.travelask.ru/system/images/files/000/336/892/wysiwyg_jpg/10452canada-landscape-map-wallpaper-3.jpg?1502197579";
+}
+
+function prev() {
+    let image = document.getElementById('photo');
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSov0YMBfaVg2v5TMR2-NFdf7mRyFmwtQEfXA&usqp=CAU";
+}
