a03269cfab8d9528648ef87cf54f35cfb841204d
Elena Kovalenko <aszhigalova@gmail.com>
2022-11-28 08:44:22 +1100

    dugs fixed

--- week 14/task1.js
+++ week 14/task1.js
@@ -1,26 +1,26 @@
 function addision() {
-    let a = document.getElementById('addOne');
-    let b = document.getElementById('addTwo'); 
-    let sum = (a.value + b.value);
+    let a = document.getElementById('addOneAd');
+    let b = document.getElementById('addTwoAd'); 
+    let sum = (+a.value + +b.value);
     document.getElementById('resultAdd').innerHTML = sum;
 }
 
 function subtraction() {
-    let a = document.getElementById('addOne');
-    let b = document.getElementById('addTwo'); 
-    let sum = (a.value - b.value);
+    let a = document.getElementById('addOneSub');
+    let b = document.getElementById('addTwoSub'); 
+    let sum = (+a.value - +b.value);
     document.getElementById('resultSub').innerHTML = sum;
 }
 
 function multiplication() {
-    let a = document.getElementById('addOne');
-    let b = document.getElementById('addTwo'); 
-    let sum = (a.value * b.value);
+    let a = document.getElementById('addOneMult');
+    let b = document.getElementById('addTwoMult'); 
+    let sum = (+a.value * +b.value);
     document.getElementById('resultMult').innerHTML = sum;
 }
 function division() {
-    let a = document.getElementById('addOne');
-    let b = document.getElementById('addTwo'); 
-    let sum = (a.value / b.value);
+    let a = document.getElementById('addOneDiv');
+    let b = document.getElementById('addTwoDiv'); 
+    let sum = (+a.value / +b.value);
     document.getElementById('resultDiv').innerHTML = sum;
 }
